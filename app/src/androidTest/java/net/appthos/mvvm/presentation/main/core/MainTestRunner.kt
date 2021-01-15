f6cc05223ffa745976a529a6a1f10005817f3639
pathos <neopathos@me.com>
2021-01-15 23:56:13 +0900

    ViewModel test code

---
+++ app/src/androidTest/java/net/appthos/mvvm/presentation/main/core/MainTestRunner.kt
@@ -0,0 +1,13 @@
+package net.appthos.mvvm.presentation.main.core
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class MainTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
