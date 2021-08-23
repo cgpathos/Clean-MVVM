4f4da4361ca4fd8450cc4600edac482438354169
pathos <neopathos@me.com>
2021-08-24 01:28:05 +0900

    프로젝트 라이브러리 최신화, repository 업데이트를 위한 리드미 초기화

--- app/src/main/java/net/appthos/mvvm/core/alias/TypeAlias.kt
+++
@@ -1,5 +0,0 @@
-package net.appthos.mvvm.core.alias
-
-// 이름이 같아 혼돈을 피하기 위해 alias 설정
-@Deprecated("ViewModel inject 방식 변경으로 사용법이 달라질꺼 같다.\n0.5.0 버전쯤 삭제하자.") typealias DaggerAssisted = com.squareup.inject.assisted.Assisted
-@Deprecated("ViewModel inject 방식 변경으로 사용법이 달라질꺼 같다.\n0.5.0 버전쯤 삭제하자.") typealias HiltAssisted = androidx.hilt.Assisted
